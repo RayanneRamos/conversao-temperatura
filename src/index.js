b88c0222004982f31653354bc49d51807f5a75bc
Rayanne Ramos <43352880+RayanneRamos@users.noreply.github.com>
2021-02-04 16:21:53 -0300

    Criando a função Celsius

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit"
+};
+
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
